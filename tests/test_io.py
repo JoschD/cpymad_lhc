2805b7a0e85df83e9aa1568f9177ceb2080ad81a
JoschD <26184899+JoschD@users.noreply.github.com>
2024-11-08 12:06:42 +0100

    test

---
+++ tests/test_io.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from cpymad_lhc.io import PathContainer
+
+THIS_DIR  = Path(__file__)
+
+def test_path_container():
+    class Paths(PathContainer):
+        this: Path = Path(__file__)
+
+    assert Paths.this == THIS_DIR
+    assert Paths.get("this", "inputs") == THIS_DIR / "inputs"
+    assert Paths.str("this") == str(THIS_DIR)
+    assert Paths.str("this", "inputs", "other") == str(THIS_DIR / "inputs" / "other")
